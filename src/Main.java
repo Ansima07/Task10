47b05bf0b679cac7d21fc2506d8e763b5763a6da
HP CA <murayansima@gmail.com>
2025-04-07 19:30:02 +0200

    Commit #1
    In NV, from line 7 to 9, I Add a demonstration to the `Main` class showing a deposit of 5000 USD to
    the number "0745678654" using the `MoneyGram` service through the
    `DepositProcessor`.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
         IPaymentService illicoService= new IllicoCash("AIN23456");
         DepositProcessor IllicoProcessor=new DepositProcessor(illicoService);
         IllicoProcessor.depositprocess("0746377654",3000);
-        
+
+        IPaymentService moneyGramService= new MoneyGram("AIN23456");
+        DepositProcessor moneyGramProcessor=new DepositProcessor(moneyGramService);
+        moneyGramProcessor.depositprocess("0745678654",5000);
     }
 }
