8ea272a3ae6393c335295cf63ce8ccddd76e7b95
HP CA <murayansima@gmail.com>
2025-04-07 17:30:06 +0200

    Commit #1
    In NV, from line 6 to 12, I Implement the `DepositMoney` method for `MoneyGram`.

--- src/MoneyGram.java
+++ src/MoneyGram.java
@@ -3,3 +3,10 @@ public class MoneyGram implements IPaymentService{
     public MoneyGram(String ClientID){
         this.ClientID=ClientID;
 }
+    @Override
+    public void DepositMoney(String PhoneNumber, double amount) {
+        System.out.println("the ClientId is :" + ClientID);
+        System.out.println("Deposit" + amount + "USD to" + PhoneNumber + "via MoneyGram Counter");
+        }
+}
+
